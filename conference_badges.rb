8eeb8178a634de3f077be1830920b8d72f946ab3
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 17:24:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,7 @@ def batch_badge_creator(speakers)
   speakers =  "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", and "Matz"
 end
 
-def assign_rooms
+def assign_rooms(speakers, rooms)
   
+  "Hello, _____! You'll be assigned to room _____!"
 end 
